65912409e4866b5a924735822e44e1b33d1d77cd
Zainan Victor Zhou <zzn@google.com>
2018-11-22 17:26:58 -0800

    Check in initial vision

--- vision.py
+++ vision.py
@@ -16,6 +16,7 @@ def process(input_path, output_path):
     if lines is not None:
         for line in lines:
             for rho, theta in line:
+
                 a = np.cos(theta)
                 b = np.sin(theta)
                 x0 = a * rho
@@ -25,15 +26,17 @@ def process(input_path, output_path):
                 x2 = int(x0 - 1000 * (-b))
                 y2 = int(y0 - 1000 * (a))
                 angle = np.arctan2(y2 - y1, x2 - x1) * 180. / np.pi
-                if angle > 0 and angle < 10:
+                if 88 < angle or a < 2:
                     cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
     cv2.imwrite(output_path, img)
     print('Wrote to ', output_path, 'after process')
 
-root_dir = '../haoshiyou-testdata/input/'
+root_dir = '../haoshiyou-testdata/images/input/tricky'
 
 for input_file_path in glob.iglob(root_dir + '**/*.jpeg', recursive=True):
     print('Input:', input_file_path)
     output_file_path = input_file_path.replace('input', 'output')
     process(input_file_path, output_file_path)
     print('Output', output_file_path)
+
+# process('../haoshiyou-testdata/images/input/tricky/tricky-8.jpeg', '../haoshiyou-testdata/images/output/tricky/tricky-8.jpeg')
