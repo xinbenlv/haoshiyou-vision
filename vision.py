aca15275dbedcba2231dbafe9d1e5494dd9acf70
Zainan Victor Zhou <zzn@google.com>
2018-11-22 16:31:53 -0800

    Add Git GLFS support

--- vision.py
+++ vision.py
@@ -13,23 +13,24 @@ def process(input_path, output_path):
     edges = cv2.Canny(gray, 0, width, apertureSize=3)
 
     lines = cv2.HoughLines(edges, 1, np.pi / 180, 200)
-    for line in lines:
-        for rho, theta in line:
-            a = np.cos(theta)
-            b = np.sin(theta)
-            x0 = a * rho
-            y0 = b * rho
-            x1 = int(x0 + 1000 * (-b))
-            y1 = int(y0 + 1000 * (a))
-            x2 = int(x0 - 1000 * (-b))
-            y2 = int(y0 - 1000 * (a))
-            angle = np.arctan2(y2 - y1, x2 - x1) * 180. / np.pi
-            if angle > 0 and angle < 10:
-                cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
+    if lines is not None:
+        for line in lines:
+            for rho, theta in line:
+                a = np.cos(theta)
+                b = np.sin(theta)
+                x0 = a * rho
+                y0 = b * rho
+                x1 = int(x0 + 1000 * (-b))
+                y1 = int(y0 + 1000 * (a))
+                x2 = int(x0 - 1000 * (-b))
+                y2 = int(y0 - 1000 * (a))
+                angle = np.arctan2(y2 - y1, x2 - x1) * 180. / np.pi
+                if angle > 0 and angle < 10:
+                    cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
     cv2.imwrite(output_path, img)
     print('Wrote to ', output_path, 'after process')
 
-root_dir = './data/input/normal'
+root_dir = '../haoshiyou-testdata/input/'
 
 for input_file_path in glob.iglob(root_dir + '**/*.jpeg', recursive=True):
     print('Input:', input_file_path)
